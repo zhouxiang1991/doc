a6694840d871c8cecdfb6c14305068aa0f274e2a
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2017-07-21 08:56:54 +0800

    修改研报日志只要是重复的就不写入

---
+++ script/auto.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git add .
+git commit -m "$@"
+git checkout develop
+git merge --no-ff -m "merge from feature/modify" feature/modify
+git branch -d feature/modify
+git push --all
+git flow feature start modify
