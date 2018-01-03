5a2f8ea82ce6a97b568c2b06dbf22c12e1792aad
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2018-01-03 17:42:24 +0800

    backup

--- shell-script/init.sh
+++ shell-script/init.sh
@@ -6,11 +6,9 @@ cp /Users/zhouxiang/doc/private/package.json .
 # cp /Users/zhouxiang/doc/private/.eslintrc.js .
 # cp /Users/zhouxiang/doc/private/.eslintignore .
 # cp /Users/zhouxiang/doc/private/jsdoc.js .
-mkdir src
 cp -rf /Users/zhouxiang/doc/private/script .
 cp -rf /Users/zhouxiang/doc/private/test .
-cp -rf /Users/zhouxiang/doc/private/lib/* ./src
-touch ./src/index.js
+cp -rf /Users/zhouxiang/doc/private/src .
 yarn install 
 git flow init -d
 # ncu -a
